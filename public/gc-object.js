dd3a0619ad352ba204d13322949b70a718fb1010
wdebowicz <wdebowicz@gmail.com>
2022-04-02 20:17:47 +0200

    destroy objects on click

--- public/gc-object.js
+++ public/gc-object.js
@@ -1,19 +1,27 @@
 class GCObject extends HTMLElement {
 
+    destroy = () => {
+        this.remove()
+    }
+
     connectedCallback() {
-        this.innerHTML = `
+        this.attachShadow({mode: "open"})
+        this.shadowRoot.innerHTML = `
             <style>
                 .gs-object {
                     height: 80px;
                     width: 80px;
                     border: 5px solid black;
                     background-color: yellow;
+                    cursor: pointer;
                 }
             </style>
             <div class="gs-object">
-                X
+                ${Math.floor(Math.random() * 100)}
             </div>
         `;
+
+        this.addEventListener('click', this.destroy);
     }
 }
 
