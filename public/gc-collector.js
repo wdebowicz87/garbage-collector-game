847478e090a50aadb102c604b8b2f9478c7e08fb
wdebowicz <wdebowicz@gmail.com>
2022-04-03 21:06:22 +0200

    animate yourself bin top

--- public/gc-collector.js
+++ public/gc-collector.js
@@ -6,64 +6,40 @@ class GcCollector extends HTMLElement {
             <style>
                 .objects-collector {
                     border: 1px solid blue;
+                    margin-top: 50px;
                 }
                 img {
-                    max-height: 100%;
-                    width: auto;
-                    position: absolute;
+                    max-width: 100%;
+                    height: auto;
                 }
-                .bin-close {
-                    opacity: 1;
-                    animation: fadeClose 0.7s alternate infinite;
-                }
-                .bin-45 {
-                    opacity: 0;
-                    animation: fade45 0.7s alternate infinite;
-                }
-                .bin-open {
-                    opacity: 0;
-                    animation: fadeOpen 0.7s alternate infinite;
-                }
-                @keyframes fadeClose {
-                    0% { opacity: 1}
-                    33% { opacity: 1}
-                    35% { opacity: 0}
-                    100% { opacity: 0}
-                }
-                @keyframes fade45 {
-                    0% { opacity: 0}
-                    31% { opacity: 0}
-                    33% { opacity: 1}
-                    66% { opacity: 1}
-                    70% { opacity: 0}
-                    100% { opacity: 0}
-                }
-                @keyframes fadeOpen {
-                    0% { opacity: 0}
-                    64% { opacity: 0}
-                    66% { opacity: 1}
-                    100% { opacity: 1}
-                }
-                
                 .bin {
                     height: 200px;
                     width: 100px;
                     position: relative;
-                    left: -100px;
-                    animation: move 20s 2s infinite;
+                    left: -150px;
+                    animation: move 20s 1s infinite;
                     animation-fill-mode: forwards;
                     rotate: -10deg;
                 }
+                .bin-top {
+                      transform-origin: bottom left;
+                      transform-style: preserve-3D;
+                      animation: rotate 1s infinite alternate;
+                }
+                @keyframes rotate {
+                    0% {transform: rotate(0deg); }
+                    100% {transform: rotate(-100deg) }
+                }
+                
                 @keyframes move {
-                    0% {left: -100px; }
+                    0% {left: -150px; }
                     100% {left: 800px; }
                 }
             </style>
             <div class="objects-collector">
                 <div class="bin">
-                    <img class="bin-close" src="images/bin_close.svg"></img>
-                    <img class="bin-45" src="images/bin_45.svg"></img>          
-                    <img class="bin-open" src="images/bin_open.svg"></img>          
+                    <img class="bin-top" src="images/bin_top.svg"></img>
+                    <img class="bin-bottom" src="images/bin.svg"></img>          
                 </div>
             </div>
         `;
